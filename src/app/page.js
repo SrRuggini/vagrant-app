6b852a568c70a4b0e2ecabea725f767ad250ec95
unknown <ruggini001@gmail.com.br>
2024-06-11 14:20:37 -0300

    2 and 3 scenarios added

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,40 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import styles from '/src/app/styles/page.module.css';
 
 export default function Home() {
     const scenarios = [
         'Shelter from the Storm',
         'Under My Skin',
-        'Paradise by the Marco Light',
+        'Paradise by the Maco Light',
     ];
     return (
         <>
             <main className={styles.main}>
                 <div className="container">
+                    <div className="Logo">
+                        <Link href={'/'}>
+                            <Image
+                                src="/logotipo.png"
+                                width={265}
+                                height={45}
+                                alt="Vagrantsong"
+                            />
+                        </Link>
+                    </div>
                     <div className="content-wrap">
                         {scenarios.map((scenario, index) => {
                             const slug = scenario.toLowerCase().replace(/ /g, '-');
                             return (
-                                <div className={styles.Scenario__Item} key={`scenario-${index}`}>
+                                <div
+                                    className={styles.Scenario__Item}
+                                    key={`scenario-${index}`}
+                                >
                                     <Link href={`/scenarios/${slug}`}>
-                                        <span className={styles.Scenario__Subtitle}>Cenário {index}</span>
+                                        <span className={styles.Scenario__Subtitle}>
+                      Cenário {index}
+                                        </span>
                                         <span className={styles.Scenario__Title}>{scenario}</span>
                                     </Link>
                                 </div>
--- src/app/scenarios/[scenario]/page.js
+++ src/app/scenarios/[scenario]/page.js
@@ -4,6 +4,8 @@ import React, { useState, useEffect } from 'react';
 import styles from '/src/app/styles/scenario.module.css';
 import { importJsonByName } from '/src/app/utils/importJson';
 import Collapsible from '/src/app/components/collapsible';
+import Link from 'next/link';
+import Image from 'next/image';
 import PropTypes from 'prop-types';
 
 export default function Scenarios({ params }) {
@@ -27,6 +29,16 @@ export default function Scenarios({ params }) {
     return (
         <main className={styles.main}>
             <div className="container">
+                <div className="Logo">
+                    <Link href={'/'}>
+                        <Image
+                            src="/logotipo.png"
+                            width={265}
+                            height={45}
+                            alt="Vagrantsong"
+                        />
+                    </Link>
+                </div>
                 {scenarioData && (
                     <>
                         <h5 className={styles.Scenario__SubTitle}>
@@ -41,9 +53,14 @@ export default function Scenarios({ params }) {
                                     __html: scenarioData?.storyDescription,
                                 }}
                             />
-                            <br/><br/>
+                            <br />
+                            <br />
                             {scenarioData?.events.map((event, index) => (
-                                <Collapsible description={event?.description} title={event?.title} key={index} />
+                                <Collapsible
+                                    description={event?.description}
+                                    title={event?.title}
+                                    key={index}
+                                />
                             ))}
                         </div>
                     </>
