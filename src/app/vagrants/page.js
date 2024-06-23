fc6e2a562dd46e3e67a6c70d1c5678f39ecce9ab
unknown <ruggini001@gmail.com.br>
2024-06-23 20:53:59 -0300

    project update

--- src/app/vagrants/page.js
+++ src/app/vagrants/page.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
 import Image from 'next/image';
 import Header from '/src/app/components/header';
 import styles from '/src/app/styles/vagrants.module.css';
